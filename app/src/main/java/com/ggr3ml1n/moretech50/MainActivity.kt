5a6235227e0d3f5369ae6fe3dd0033f742f28284
Dinar Subeev <dinars163@gmail.com>
2023-10-14 11:55:18 +0400

    Создан проект

---
+++ app/src/main/java/com/ggr3ml1n/moretech50/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ggr3ml1n.moretech50
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
