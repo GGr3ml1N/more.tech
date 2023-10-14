efbb7ac911474cd98d7ce026b2f582258956402f
Dinar Subeev <dinars163@gmail.com>
2023-10-14 15:57:45 +0400

    SplashScreen и MainViewModel

---
+++ app/src/main/java/com/ggr3ml1n/moretech50/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.ggr3ml1n.moretech50
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            _isLoading.value = false
+        }
+    }
+}
