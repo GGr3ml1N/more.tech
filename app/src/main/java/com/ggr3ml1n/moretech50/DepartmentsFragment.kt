fd56ab2636388004a5fa1ac7ad58e9e3ddb99658
xp62hzsc <xp62hzsc@duck.com>
2023-10-15 08:41:08 +0300

    Оптимизация

--- app/src/main/java/com/ggr3ml1n/moretech50/DepartmentsFragment.kt
+++ app/src/main/java/com/ggr3ml1n/moretech50/DepartmentsFragment.kt
@@ -1,10 +1,11 @@
 package com.ggr3ml1n.moretech50
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
 class DepartmentsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
